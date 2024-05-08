36d63d5edd7da2803acc0ac389706caf64170fb8
Oscar V <oscar.valenzuela.b@gmail.com>
2024-05-08 13:23:34 -0700

    including new datasets in line of the LM verification code

--- setup.py
+++ setup.py
@@ -3,13 +3,26 @@ from setuptools import setup, find_packages
 setup(
     name='xmonkey_namonica',
     version='0.1.8',
-    packages=find_packages(),
+    author="Oscar Valenzuela",
+    author_email="oscar.valenzuela.b@gmail.com",
+    url='https://github.com/Xpertians/xmonkey-namonica',
+    license='Apache 2.0',
     entry_points={
         'console_scripts': [
             'xmonkey-namonica = xmonkey_namonica.cli:main'
         ]
     },
     python_requires='>=3.6',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'xmonkey_namonica': ['datasets/*.pkl'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
     install_requires=[
         "spacy",
         "nltk",
