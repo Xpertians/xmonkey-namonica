e70df5a66b242eb19c21e74aa37f0fa020e08536
Oscar V <oscar.valenzuela.b@gmail.com>
2024-05-20 16:50:19 -0700

    small fix and version upgrade

--- xmonkey_namonica/handlers/golang_handler.py
+++ xmonkey_namonica/handlers/golang_handler.py
@@ -76,8 +76,14 @@ class GolangHandler(BaseHandler):
         if "@" in go_pkg:
             go_pkg, version = go_pkg.split("@")
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'User-Agent': (
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) '
+                'Gecko/20100101 Firefox/106.0'
+            ),
+            'Accept': (
+                'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                'image/avif,image/webp,*/*;q=0.8'
+            ),
             'Accept-Language': 'en-US,en;q=0.5',
             'DNT': '1',
             'Connection': 'keep-alive',
@@ -199,8 +205,14 @@ class GolangHandler(BaseHandler):
 
     def fetch_file(self, url):
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'User-Agent': (
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) '
+                'Gecko/20100101 Firefox/106.0'
+            ),
+            'Accept': (
+                'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                'image/avif,image/webp,*/*;q=0.8'
+            ),
             'Accept-Language': 'en-US,en;q=0.5',
             'DNT': '1',
             'Connection': 'keep-alive',
